a7e5f7223b92a381dba0bc51b35a5c35be6dd461
Omar <1058647@hr.nl>
2023-10-25 11:19:49 -0400

    handle_error() finished and setup() omar's part finished waiting on Gus

--- assignment.sh
+++ assignment.sh
@@ -2,19 +2,20 @@
 
 # Define global variable here
 
-# TODO: Add required and additional packagenas dependecies 
+# TODO: : Add required and additional packagenas dependecies 
 # for your implementation
 # declare -a packages=()
 
 # Omar
-# TODO: define a function to handle errors
+# TODO: : define a function to handle errors
 # This funtion accepts two parameters one as the error message and one as the command to be excecuted when error occurs.
 function handle_error() {
     # Do not remove next line!
     echo "function handle_error"
 
-   # TODO Display error and return an exit code
-   
+   # TODO:  Display error and return an exit code
+    echo "Error: $1"
+    exit ${2:-1}
 }
 
 # Omar
@@ -23,48 +24,81 @@ function setup() {
     # Do not remove next line!
     echo "function setup"
 
-    # TODO check if nessassary dependecies and folder structure exists and 
+    # TODO:  check if nessassary dependecies and folder structure exists and 
     # print the outcome for each checking step
-    
-    # TODO installation from online package requires values for
-    # package_name package_url install_dir
-
-    # TODO check if required dependency is not already installed otherwise install it
+    # TODO:  check if required dependency is not already installed otherwise install it
     # if a a problem occur during the this process 
     # use the function handle_error() to print a messgage and handle the error
+    echo ""
+    echo "Checking dependencies..."
+    dependencies=("unzip" "wget" "curl")
+    for dependency in "${dependencies[@]}"; do
+        if ! command -v "$dependency" >/dev/null 2>&1; then
+            echo "$dependency is not installed, installing..."
 
-    # TODO check if required folders and files exists before installations
+            # Install dependency
+            if ! sudo apt-get install -y "$dependency"; then
+                handle_error "Failed to install $dependency"
+            fi
+        
+            echo "$dependency installation finished"
+        else
+            echo "$dependency is installed"
+        fi
+    done
+    
+    
+    # TODO:  check if required folders and files exists before installations
     # For example: the folder ./apps/ and the file "dev.conf"
+    echo ""
+    echo "Checking folders and files..."
+    if [ ! -d ~/apps ]; then
+    echo "Creating ~/apps directory..."
+    mkdir ~/apps
+    fi
+
+    if [ ! -f ~/apps/dev.conf ]; then
+        echo "Creating ~/apps/dev.conf file..."
+        touch ~/apps/dev.conf
+    fi
+
+    echo "Process complete"
+
+    # TODO:  installation from online package requires values for
+    # package_name package_url install_dir
+    
+
+
 
 }
 
 # Guus
 # Function to install a package from a URL
-# TODO assign the required parameter needed for the logic
+# TODO:  assign the required parameter needed for the logic
 # complete the implementation of the following function.
 function install_package() {
     # Do not remove next line!
     echo "function install_package"
 
-    # TODO The logic for downloading from a URL and unizpping the downloaded files of different applications must be generic
+    # TODO:  The logic for downloading from a URL and unizpping the downloaded files of different applications must be generic
 
-    # TODO Specific actions that need to be taken for a specific application during this process should be handeld in a separate if-else
+    # TODO:  Specific actions that need to be taken for a specific application during this process should be handeld in a separate if-else
 
-    # TODO Every intermediate steps need to be handeld carefully. error handeling should be dealt with using handle_error() and/or rolleback()
+    # TODO:  Every intermediate steps need to be handeld carefully. error handeling should be dealt with using handle_error() and/or rolleback()
 
-    # TODO If a file is downloaded but cannot be zipped a rollback is needed to be able to start from scratch
+    # TODO:  If a file is downloaded but cannot be zipped a rollback is needed to be able to start from scratch
     # for example: package names and urls that are needed are passed or extracted from the config file
 
-    # TODO check if the application-folder and the url of the dependency exist
-    # TODO create a specific installation folder for the current package
+    # TODO:  check if the application-folder and the url of the dependency exist
+    # TODO:  create a specific installation folder for the current package
 
-    # TODO Download and unzip the package
+    # TODO:  Download and unzip the package
     # if a a problem occur during the this proces use the function handle_error() to print a messgage and handle the error
 
-    # TODO extract the package to the installation folder and store it into a dedicated folder
+    # TODO:  extract the package to the installation folder and store it into a dedicated folder
     # If a problem occur during the this proces use the function handle_error() to print a messgage and handle the error
 
-    # TODO this section can be used to implement application specifc logic
+    # TODO:  this section can be used to implement application specifc logic
     # nosecrets might have additional commands that needs to be executed
     # make sure the user is allowed to remove this folder during uninstall
 
@@ -75,7 +109,7 @@ function rollback_nosecrets() {
     # Do not remove next line!
     echo "function rollback_nosecrets"
 
-    # TODO rollback intermiediate steps when installation fails
+    # TODO:  rollback intermiediate steps when installation fails
 }
 
 # Guus
@@ -83,7 +117,7 @@ function rollback_pywebserver() {
     # Do not remove next line!
     echo "function rollback_pywebserver"
 
-    # TODO rollback intermiediate steps when installation fails
+    # TODO:  rollback intermiediate steps when installation fails
 }
 
 # Omar
@@ -91,7 +125,7 @@ function test_nosecrets() {
     # Do not remove next line!
     echo "function test_nosecrets"
 
-    # TODO test nosecrets
+    # TODO:  test nosecrets
     # kill this webserver process after it has finished its job
 
 }
@@ -101,7 +135,7 @@ function test_pywebserver() {
     # Do not remove next line!
     echo "function test_pywebserver"    
 
-    # TODO test the webserver
+    # TODO:  test the webserver
     # server and port number must be extracted from config.conf
     # test data must be read from test.json  
     # kill this webserver process after it has finished its job
@@ -113,17 +147,17 @@ function uninstall_nosecrets() {
     # Do not remove next line!
     echo "function uninstall_nosecrets"  
 
-    #TODO uninstall nosecrets application
+    #TODO:  uninstall nosecrets application
 }
 
 # Guus
 function uninstall_pywebserver() {
     echo "function uninstall_pywebserver"    
-    #TODO uninstall pywebserver application
+    #TODO:  uninstall pywebserver application
 }
 
 # Omar
-#TODO removing installed dependency during setup() and restoring the folder structure to original state
+#TODO:  removing installed dependency during setup() and restoring the folder structure to original state
 function remove() {
     # Do not remove next line!
     echo "function remove"
@@ -137,7 +171,7 @@ function main() {
     # Do not remove next line!
     echo "function main"
 
-    # TODO
+    # TODO: 
     # Read global variables from configfile
 
     # Get arguments from the commandline
@@ -150,11 +184,12 @@ function main() {
     # bash must exit if value does not match one of those values
 
     # Execute the appropriate command based on the arguments
-    # TODO In case of setup
+    # TODO:  In case of setup
     # excute the function check_dependency and provide necessary arguments
     # expected arguments are the installation directory specified in dev.conf
 
 }
 
 # Pass commandline arguments to function main
-main "$@"
+# main "$@"
+setup
