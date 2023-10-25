a875c6cfec82a20e6bf0b942e3add777ee21a717
ZoutigeWolf <guuskamphuis@gmail.com>
2023-10-25 20:03:41 +0200

    install_package

--- assignment.sh
+++ assignment.sh
@@ -80,28 +80,73 @@ function install_package() {
     # Do not remove next line!
     echo "function install_package"
 
+    if [ $# -ne 3 ]; then
+        handle_error "Invalid number of arguments"
+    fi
+
+    package_name = $1
+    package_url = $2
+    install_dir = $3
+
     # TODO:  The logic for downloading from a URL and unizpping the downloaded files of different applications must be generic
 
     # TODO:  Specific actions that need to be taken for a specific application during this process should be handeld in a separate if-else
 
-    # TODO:  Every intermediate steps need to be handeld carefully. error handeling should be dealt with using handle_error() and/or rolleback()
+    # TODO:  Every intermediate steps need to be handeld carefully. error handeling should be dealt with using handle_error() and/or rollback()
 
     # TODO:  If a file is downloaded but cannot be zipped a rollback is needed to be able to start from scratch
     # for example: package names and urls that are needed are passed or extracted from the config file
 
     # TODO:  check if the application-folder and the url of the dependency exist
+    echo "Validating install dir..."
+    if [ ! -d $install_dir ]; then
+        handle_error "Install dir not found"
+    fi
+
+    echo "Validating package URL..."
+    if ! wget -q --method=HEAD $package_url; then
+        handle_error "Package URL is invalid"
+    fi
+
     # TODO:  create a specific installation folder for the current package
+    echo "Creating package dir..."
+    mkdir "$install_dir/$package_name"
 
     # TODO:  Download and unzip the package
     # if a a problem occur during the this proces use the function handle_error() to print a messgage and handle the error
+    echo "Downloading package..."
+    if ! wget -O "$package_name.zip" package_url; then
+        handle_error "Failed to download package"
+    fi
 
     # TODO:  extract the package to the installation folder and store it into a dedicated folder
     # If a problem occur during the this proces use the function handle_error() to print a messgage and handle the error
+    echo "Unzipping files..."
+    if ! unzip -j "$package_name.zip" -d "$install_dir/$package_name"; then
+        handle_error "Failed to unzip package"
+    fi
+
+    rm "$package_name.zip"
 
     # TODO:  this section can be used to implement application specifc logic
     # nosecrets might have additional commands that needs to be executed
     # make sure the user is allowed to remove this folder during uninstall
+    if [ $package_name == "nosecrets" ]; then
+        echo "Running nosecrets extra installation steps"
+        cd "$install_dir/$package_name"
 
+        if ! make nms; then
+            handle_error "Failed to make nms"
+        fi
+
+        if ! sudo make install; then
+            handle_error "Failed to install"
+        fi
+
+        echo "Extra installation steps completed"
+    fi
+
+    echo "Installed $package_name"
 }
 
 # Omar
@@ -192,4 +237,5 @@ function main() {
 
 # Pass commandline arguments to function main
 # main "$@"
-setup
+install_package "webserver" "https://github.com/nickjj/webserver/archive/refs/heads/master.zip" "./apps"
+install_package "nosecrets" "https://github.com/bartobri/no-more-secrets/archive/refs/heads/master.zip" "./apps"
